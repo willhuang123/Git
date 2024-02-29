1d6767da1a5b4ef9eeb82e9b1dad5a6cdc2ba6fe
will <williamhuang0017@gmail.com>
2024-02-29 11:21:37 +0800

    Add WriteLine in Program.cs

--- Member1/Member1/Program.cs
+++ Member1/Member1/Program.cs
@@ -11,6 +11,7 @@ namespace Member1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello git!");
+            Console.WriteLine("member2 modified.");
             Console.ReadKey();
         }
     }
